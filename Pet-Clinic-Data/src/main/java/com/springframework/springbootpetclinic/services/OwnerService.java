851721a2ac60e424360d51b1adb751b691ecab91
Anna Łuczak <an.archeo@gmail.com>
2022-01-02 20:07:40 +0000

    Created Base Interface and refactored services. Closes #11

--- Pet-Clinic-Data/src/main/java/com/springframework/springbootpetclinic/services/OwnerService.java
+++ Pet-Clinic-Data/src/main/java/com/springframework/springbootpetclinic/services/OwnerService.java
@@ -2,12 +2,8 @@ package com.springframework.springbootpetclinic.services;
 
 import com.springframework.springbootpetclinic.model.Owner;
 
-import java.util.Set;
 
-public interface OwnerService {
+public interface OwnerService extends CrudService<Owner, Long>{
 
-    Owner findById(Long id);
-    Owner save(Owner owner);
-    Set<Owner> findAll();
     Owner findByLastName(String lastName);
 }
