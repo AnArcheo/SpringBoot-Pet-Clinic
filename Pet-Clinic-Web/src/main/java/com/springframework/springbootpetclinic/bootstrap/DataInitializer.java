5fd6be8c783d2d284265a6032da874b9579171c7
Anna Łuczak <an.archeo@gmail.com>
2022-01-03 23:16:40 +0000

    Updated Map implementation to manage setting ID property. Closes #23

--- Pet-Clinic-Web/src/main/java/com/springframework/springbootpetclinic/bootstrap/DataInitializer.java
+++ Pet-Clinic-Web/src/main/java/com/springframework/springbootpetclinic/bootstrap/DataInitializer.java
@@ -24,14 +24,12 @@ public class DataInitializer implements CommandLineRunner {
     public void run(String... args) throws Exception {
 
         Owner owner1 = new Owner();
-        owner1.setId(1L);
         owner1.setFirstName("Tom");
         owner1.setLastName("Mitchel");
 
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
-        owner2.setId(2L);
         owner2.setFirstName("An");
         owner2.setLastName("Looney");
 
@@ -40,14 +38,12 @@ public class DataInitializer implements CommandLineRunner {
         System.out.println("Initialized Owners--------");
 
         Vet vet1 = new Vet();
-        vet1.setId(1L);
         vet1.setFirstName("Samuel");
         vet1.setLastName("Rust");
 
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
-        vet2.setId(2L);
         vet2.setFirstName("Amanda");
         vet2.setLastName("Cool");
 
