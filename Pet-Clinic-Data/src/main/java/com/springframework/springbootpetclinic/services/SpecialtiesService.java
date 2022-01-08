19a5dacb90a576da4172a10ce4b5263bcd28a7fd
Anna Łuczak <an.archeo@gmail.com>
2022-01-08 23:11:25 +0000

    Created Specialty Map Service Implementation. Closes #33

---
+++ Pet-Clinic-Data/src/main/java/com/springframework/springbootpetclinic/services/SpecialtiesService.java
@@ -0,0 +1,6 @@
+package com.springframework.springbootpetclinic.services;
+
+import com.springframework.springbootpetclinic.model.Speciality;
+
+public interface SpecialtiesService extends CrudService<Speciality, Long>{
+}
