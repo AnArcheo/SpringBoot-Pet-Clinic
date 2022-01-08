10290c6bc38faaa44256b754ca01b75e512e8b9e
Anna Łuczak <an.archeo@gmail.com>
2022-01-08 22:50:21 +0000

    Added Contact Info and Pets properties on Bootstrap class to Owners. Closes #35

--- Pet-Clinic-Web/src/main/java/com/springframework/springbootpetclinic/bootstrap/DataInitializer.java
+++ Pet-Clinic-Web/src/main/java/com/springframework/springbootpetclinic/bootstrap/DataInitializer.java
@@ -1,6 +1,7 @@
 package com.springframework.springbootpetclinic.bootstrap;
 
 import com.springframework.springbootpetclinic.model.Owner;
+import com.springframework.springbootpetclinic.model.Pet;
 import com.springframework.springbootpetclinic.model.PetType;
 import com.springframework.springbootpetclinic.model.Vet;
 import com.springframework.springbootpetclinic.services.OwnerService;
@@ -8,6 +9,8 @@ import com.springframework.springbootpetclinic.services.PetTypeService;
 import com.springframework.springbootpetclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
 
 
 @Component
@@ -38,16 +41,36 @@ public class DataInitializer implements CommandLineRunner {
         Owner owner1 = new Owner();
         owner1.setFirstName("Tom");
         owner1.setLastName("Mitchel");
-
+        owner1.setAddress("4 View Lane");
+        owner1.setCity("Dublin");
+        owner1.setTelephone("12354367");
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
         owner2.setFirstName("An");
         owner2.setLastName("Looney");
-
+        owner2.setAddress("21 Fox Park");
+        owner2.setCity("Cork");
+        owner2.setTelephone("568942341");
         ownerService.save(owner2);
 
         System.out.println("Initialized Owners--------");
+
+        Pet pet1 = new Pet();
+        pet1.setPetType(savedPetType1);
+        pet1.setOwner(owner1);
+        pet1.setBirthDate(LocalDate.now());
+        pet1.setName("Mikey");
+        owner1.getPets().add(pet1);
+
+        Pet pet2 = new Pet();
+        pet2.setPetType(savedPetType2);
+        pet2.setOwner(owner2);
+        pet2.setBirthDate(LocalDate.now());
+        pet2.setName("Mira");
+        owner2.getPets().add(pet2);
+
+        System.out.println("Initialized Pets--------");
 
         Vet vet1 = new Vet();
         vet1.setFirstName("Samuel");
