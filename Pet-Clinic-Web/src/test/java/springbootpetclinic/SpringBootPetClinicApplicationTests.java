ef729d5670e1772558c3be068b471b6199c3e04e
Anna Łuczak <an.archeo@gmail.com>
2022-01-18 22:43:57 +0000

    fixed issue with contextLoads() while running test application

--- Pet-Clinic-Web/src/test/java/springbootpetclinic/SpringBootPetClinicApplicationTests.java
+++ Pet-Clinic-Web/src/test/java/springbootpetclinic/SpringBootPetClinicApplicationTests.java
@@ -1,11 +1,14 @@
 package springbootpetclinic;
 
 
+import com.springframework.springbootpetclinic.SpringBootPetClinicApplication;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-
-@SpringBootTest
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(classes = SpringBootPetClinicApplication.class) // without it it will fail run contextLoads()
 class SpringBootPetClinicApplicationTests {
 
     @Test
