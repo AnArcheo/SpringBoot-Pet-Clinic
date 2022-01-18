c56756b022ffd06e6675fd18816f859eb2025660
Anna Łuczak <an.archeo@gmail.com>
2022-01-18 00:00:03 +0000

    Checked juint5 is setup. Closes #6

--- Pet-Clinic-Web/src/test/java/springbootpetclinic/SpringBootPetClinicApplicationTests.java
+++ Pet-Clinic-Web/src/test/java/springbootpetclinic/SpringBootPetClinicApplicationTests.java
@@ -1,7 +1,9 @@
 package springbootpetclinic;
 
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
 
 @SpringBootTest
 class SpringBootPetClinicApplicationTests {
