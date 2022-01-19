dfef16562fd1a07de346579e4eec8f53f521e3c0
Anna Łuczak <an.archeo@gmail.com>
2022-01-19 23:45:47 +0000

    Added CRUD Tests for Owner SD JPA Service.

--- Pet-Clinic-Data/src/test/java/com/springframework/springbootpetclinic/services/springdatajpa/OwnerSDJpaServiceTest.java
+++ Pet-Clinic-Data/src/test/java/com/springframework/springbootpetclinic/services/springdatajpa/OwnerSDJpaServiceTest.java
@@ -1,28 +1,36 @@
 package com.springframework.springbootpetclinic.services.springdatajpa;
 
+import com.springframework.springbootpetclinic.model.Owner;
 import com.springframework.springbootpetclinic.repositories.OwnerRepository;
 import com.springframework.springbootpetclinic.repositories.PetRepository;
 import com.springframework.springbootpetclinic.repositories.PetTypeRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
 class OwnerSDJpaServiceTest {
 
+    @Mock
     OwnerRepository ownerRepository;
+    @Mock
     PetRepository petRepository;
+    @Mock
     PetTypeRepository petTypeRepository;
-
-    OwnerSDJpaService owner;
+    @InjectMocks
+    OwnerSDJpaService ownerSDJpaService;
 
     @BeforeEach
     void setUp() {
+
     }
 
     @Test
     void findByLastName() {
+        Owner smith = ownerSDJpaService.findByLastName("Smith");
     }
 
     @Test
