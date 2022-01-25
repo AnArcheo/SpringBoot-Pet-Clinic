cd7be903d8b1a8e2471e91b9b94e12b910ac063c
Anna Łuczak <an.archeo@gmail.com>
2022-01-25 23:29:39 +0000

    Added MockMvc for Owner Controller

--- Pet-Clinic-Web/src/test/java/com/springframework/springbootpetclinic/controllers/OwnerControllerTest.java
+++ Pet-Clinic-Web/src/test/java/com/springframework/springbootpetclinic/controllers/OwnerControllerTest.java
@@ -1,5 +1,6 @@
 package com.springframework.springbootpetclinic.controllers;
 
+import com.springframework.springbootpetclinic.model.Owner;
 import com.springframework.springbootpetclinic.services.OwnerService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -7,9 +8,17 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @ExtendWith(MockitoExtension.class)
 class OwnerControllerTest {
+
+    Set<Owner> owners;
+    MockMvc mockMvc;
 
     @Mock
     OwnerService ownerService;
@@ -19,6 +28,11 @@ class OwnerControllerTest {
 
     @BeforeEach
     void setUp() {
+        owners = new HashSet<>();
+        owners.add(Owner.builder().id(1L).build());
+        owners.add(Owner.builder().id(2L).build());
+
+        mockMvc = MockMvcBuilders.standaloneSetup(ownerController).build();
     }
 
     @Test
