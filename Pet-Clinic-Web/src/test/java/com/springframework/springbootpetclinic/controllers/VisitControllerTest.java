fe390ccfdcebe3109e66de6863878a9131749a54
Anna Łuczak <an.archeo@gmail.com>
2022-02-01 23:47:05 +0000

    Created Spring MockMVC VistController test base class. #72

---
+++ Pet-Clinic-Web/src/test/java/com/springframework/springbootpetclinic/controllers/VisitControllerTest.java
@@ -0,0 +1,27 @@
+package com.springframework.springbootpetclinic.controllers;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class VisitControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void setAllowedFields() {
+    }
+
+    @Test
+    void loadPetWithVisit() {
+    }
+
+    @Test
+    void initNewVisitForm() {
+    }
+
+    @Test
+    void processNewVisitForm() {
+    }
+}
