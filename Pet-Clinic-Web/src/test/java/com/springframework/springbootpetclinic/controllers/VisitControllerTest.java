6136ee5b317b54ca23fac5e358dec2694b0d0c17
Anna Łuczak <an.archeo@gmail.com>
2022-02-03 22:55:44 +0000

    Fixed bug for Pet Visit Date validation. Closes #71

--- Pet-Clinic-Web/src/test/java/com/springframework/springbootpetclinic/controllers/VisitControllerTest.java
+++ Pet-Clinic-Web/src/test/java/com/springframework/springbootpetclinic/controllers/VisitControllerTest.java
@@ -2,8 +2,13 @@ package com.springframework.springbootpetclinic.controllers;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+@ExtendWith(MockitoExtension.class)
 class VisitControllerTest {
+
+
 
     @BeforeEach
     void setUp() {
